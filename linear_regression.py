68441b6e684e6c5e4a65404edce43cfbd18da602
Ricardo <ricardofs6@hotmail.com>
2026-01-12 21:56:48 +0000

    Add LinearRegression class structure

---
+++ linear_regression.py
@@ -0,0 +1,48 @@
+import numpy as np
+
+
+class LinearRegression:
+    """
+    Linear Regression implemented from scratch using Gradient Descent.
+
+    This class allows:
+    - fitting a linear model to data
+    - predicting new values
+    """
+
+    def __init__(self, learning_rate=0.01, n_iterations=1000):
+        """
+        Initialize the model parameters.
+
+        Parameters:
+        - learning_rate: step size for gradient descent
+        - n_iterations: number of iterations for training
+        """
+        self.learning_rate = learning_rate
+        self.n_iterations = n_iterations
+
+        # Model parameters (to be learned during training)
+        self.weight = None
+        self.bias = None
+
+    def fit(self, X, y):
+        """
+        Train the linear regression model using gradient descent.
+
+        Steps (high-level):
+        1. Initialize weight and bias
+        2. Loop over the number of iterations
+        3. Compute predictions
+        4. Compute error
+        5. Update weight and bias
+        """
+        pass  # to be implemented
+
+    def predict(self, X):
+        """
+        Predict output values for given input data X.
+
+        Returns:
+        - Predicted values
+        """
+        pass  # to be implemented
